5565f455ff951ba15e088d1e5345d2b5542bdf1e
Matt Carney <matt.carney50@gmail.com>
2023-06-27 00:41:16 -0400

    DLR calc drafted, progress on contour visual

--- pages/02_Pobabilistic Weather - Sensitivity.py
+++ pages/02_Pobabilistic Weather - Sensitivity.py
@@ -2,18 +2,99 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from scipy.stats import norm
+
+p = np.arange(0.1,1,.1)
+
+#n_0 = norm.ppf(p, loc=25, scale=4)
+#n_1= norm.ppf(p, loc=25, scale=0.ZZ)
+
+
+
+def gen_normal(mu=5, sigma=0.1, num=500):
+    return np.random.normal(loc=mu, scale=sigma, size=num)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=[1, 2, 3, 4, 5], y=[7, 7, 7, 7, 7], fill='tozeroy', fillcolor='purple',
+                    mode='none', line_shape='spline' # override default markers+lines
+                    ))
+
+fig.add_trace(go.Scatter(x=[1, 2, 3, 4], y=[6, 5, 6, 5], fill='tozeroy', fillcolor='red',
+                    mode= 'none', line_shape='spline'))
+
+fig.add_trace(go.Scatter(x=[1, 2, 3], y=[3, 4, 3], fill='tozeroy', fillcolor='green',
+                    mode='none', line_shape='spline' # override default markers+lines
+                    ))
+fig.add_trace(go.Scatter(x=[1, 2, 3], y=[2, 1, 2], fill='tozeroy', fillcolor='red',
+                    mode='none', line_shape='spline' # override default markers+lines
+                    ))
+fig.add_trace(go.Scatter(x=[1, 2, 3, 4], y=[1, 1, 1, 1], fill='tozeroy', fillcolor='purple',
+                    mode='none', line_shape='spline'  # override default markers+lines
+                    ))
+
+
+# x = np.array([1, 2, 3, 4, 5])
+# y = np.array([1, 3, 2, 3, 1])
+
+
+
+# fig = go.Figure()
+
+# fig.add_trace(go.Scatter(x=x, y=y + 5, name="spline",fill='tozeroy', fillcolor='purple',
+#                     #text=["tweak line smoothness<br>with 'smoothing' in line object"],
+#                     #hoverinfo='text+name',
+#                     line_shape='spline'))
+
+
+
+
+
+
+
+
+st.plotly_chart(fig)
+
+
+
+y_1 = gen_normal(sigma=0.5, num = 10000)
+x_1 = [1 for x in range(len(y_1))]
+y_2 = gen_normal(mu=5.1, sigma=0.5,num=10000)
+x_2 = [1.1 for x in range(len(y_1))]
+y_3 = gen_normal(mu=5,sigma=0.5, num=10000)
+x_3 = [1.2 for x in range(len(y_1))]
+y = np.array([y_1, y_2, y_3]).flatten('F')
+x = np.array([x_1, x_2, x_3]).flatten('F')
+df = pd.DataFrame({'y': y, 'x': x})
+#fig = px.density_contour(df, x="x", y="y")
+#fig.update_traces(contours_coloring="fill", contours_showlabels = True)
+
+len(x)
+fig = go.Figure(go.Histogram2dContour(
+        x = x,
+        y = y,
+        colorscale = 'jet'
+        ))
+fig.update_layout(xaxis_range=[1,1.2])
+st.plotly_chart(fig)
+
+
+
+
+
 
 
 #np.random.seed(123)
-mean = st.sidebar.slider('mean', 0, 10, 5)
+mean = st.sidebar.slider('temp_high', 1, 10, 5)
 variance = st.sidebar.slider('variance', 0.0, 5.0, .1)
-mean1 = st.sidebar.slider('mean1', 0, 10, 5)
+mean1 = st.sidebar.slider('temp_low', int(0), int(mean), int(mean/2))
 variance1 = st.sidebar.slider('variance1', 0.0, 5.0, .1)
 mean2 = st.sidebar.slider('mean2', 0, 10, 5)
 variance2 = st.sidebar.slider('variance2', 0.0, 5.0, .1)
 
-def gen_normal(mu=5, sigma=0.1, num=500):
-    return np.random.normal(loc=mu, scale=sigma, size=num)
+
 
 norm = gen_normal(mu=mean, sigma=variance)
 
--- utils.py
+++ utils.py
@@ -1,27 +1,71 @@
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 
-q_c1, q_c2 = 0, 0
-math.pi
+high = 30
+low = 20
+amp = (high-low)/2
+ave = (high+low)/2
+len(t)
+t = np.arange(3, 27)
+vals = np.sin(t) * amp + ave
+
+fig, ax = plt.subplots()
+ax.plot(t, vals, 'green')
+plt.show()
+plt.close()
+vals
+
+len(t)
+len(vals)
+
+time = np.arange(0., 10., 0.2)
+velocity = np.sin(time) * 10
+distance = np.zeros_like(time, dtype=float)
+
+fig, ax = plt.subplots()
+#ax1.set_ylabel("distance (m)")
+#ax1.set_xlabel("time")
+ax.plot(t, vals, "blue")
+
+plt.show()
+
+
+
+
+
 
 class DLR:
     """
+    Returns the DLR Ampacity, I, based on IEEE 738-2006
     Steps:
-    1.) Calculate T_film variable calculated
-    2.) Fluid properties, u_f, p_f, k_f calculated
-    3.) 
+    1.) Calculate film temperature: T_film 
+    2.) Calculate fuid properties: u_f, p_f, k_f
+    3.) Calculate wind direction factor: K_ang
+    4.) Calculaate convection heat loss
+    5.) Calculate radiated heat loss
+    6.) Calculate solar head gain
+    7.) DLR Ampactity
+
+    Args:
+    wind_speed: m/s
+    wing_angle: deg from line
+    ambient_temp: deg C
+    eff_rad_had_flux: W/m^s, already compensated for effective angle of incidence of the sun's rays
+ 
     """
     
     
-    def __init__(self, wind_speed, wind_angle, ambient_temp):
+    def __init__(self, wind_speed, wind_angle, ambient_temp, eff_rad_heat_flux):
         # These could be class variables, but access slightly faster as per-instance variables
         self.T_c = 100 # Max allowable temp, celcius
         self.D = 28.11 # Conductor outside diameter, mm
         self.elevation = 0
-
+        self.Ksolar = 1.0 
         self.E = 0.5 # Emissivity 
+        self.alpha = 0.5 # solar absorptivity
 
         # Conductor AC resistance
         self.T_low = 25
@@ -33,6 +77,7 @@ class DLR:
         self.w_speed = wind_speed
         self.w_angle = wind_angle * (math.pi/180) # Convert degres to radians   
         self.T_amb = ambient_temp # Ambient temp, celcius
+        self.Q_ef = eff_rad_heat_flux
 
         self.T_f = (self.T_c + self.T_amb)/2 # Film temperature
 
@@ -57,46 +102,57 @@ class DLR:
         return u_f, p_f, k_f
 
     def wind_direction_factor(self):
-        Kang = 1.194 - math.cos(self.wind_angle) + 0.194 * math.cos(2*self.wind_angle) + 0.368 * math.sin(2*self.wind_angle)
-        return Kang
+        K_ang = 1.194 - math.cos(self.w_angle) + 0.194 * math.cos(2*self.w_angle) + 0.368 * math.sin(2*self.w_angle)
+        return K_ang
 
 
-    def convection_heat_loss(self, p_f, u_f, k_f, Kang):
+    def convection_heat_loss(self, p_f, u_f, k_f, K_ang): 
         # Natural convection (no wind)
         q_nat = 0.02050 * p_f**0.5 * self.D**0.75 * (self.T_c - self.T_amb)**1.25 # W/m
 
         # Forced convection heat loss
-        if self.wind_speed > 0: 
+        if self.w_speed > 0: 
             # low wind speed
-            q_c1 = (1.01 + 0.0372*((self.D * p_f * self.w_speed)/(u_f))**0.52) * k_f * Kang * (self.T_c - self.T_a)
+            q_c1 = (1.01 + 0.0372*((self.D * p_f * self.w_speed)/(u_f))**0.52) * k_f * K_ang * (self.T_c - self.T_amb)
             
             # high wind speed
-            q_c2 = (0.0119*((self.D * p_f * self.w_speed)/(u_f))**0.6) * k_f * Kang * (self.T_c - self.T_a)
+            q_c2 = (0.0119*((self.D * p_f * self.w_speed)/(u_f))**0.6) * k_f * K_ang * (self.T_c - self.T_amb)
         else:
             q_c1, q_c2 = 0, 0
                 
         return max(q_nat, q_c1, q_c2) # always use max
 
-    def radiated_heat_loss(self):
+    def radiated_heat_loss(self): #qr
         q_r = 0.0178 * self.D * self.E * (((self.T_c + 273)/100)**4 - ((self.T_amb + 273)/100)**4)
         return q_r        
 
+    def solar_heat_gain(self): 
+        self.Q_ef = self.Q_ef * self.Ksolar # Elevation correction factor
+        q_s = self.alpha * self.Q_ef * self.D/1000
+        return q_s
 
     def resistance(self):
         R_Tc = (self.R_high - self.R_low)/(self.T_high - self.T_low)*(self.T_c-self.T_low) + self.R_low
         return R_Tc
 
-    def runner(self):
+    def ampacity(self):
         u_f, p_f, k_f = self.fluid_propertires() # fluid properties
-        Kang = self.wind_direction_factor() # Wind direction factor
-        q_c = self.convection_heat_loss(p_f, u_f, k_f, Kang)
+        K_ang = self.wind_direction_factor() # Wind direction factor
+        q_c = self.convection_heat_loss(p_f, u_f, k_f, K_ang)
         q_r = self.radiated_heat_loss()
+        q_s = self.solar_heat_gain()
+        R_Tc = self.resistance()
 
-        R_Tc = self.resistance() 
+        I = math.sqrt((q_c + q_r - q_s)/R_Tc)
+
+        return I
+        
 
 
+a = DLR(wind_speed=5, wind_angle=90, ambient_temp=20, eff_rad_heat_flux=1000)
+d = []
+for i in range(1000):
+    a = DLR(wind_speed=5, wind_angle=90, ambient_temp=20, eff_rad_heat_flux=1000)
+    d.append(a.ampacity())
 
-
-a = DLR
-a.head_loss()
-a.resistance()
+a.ampacity()
