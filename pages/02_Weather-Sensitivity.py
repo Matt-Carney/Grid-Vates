435d324af7bee1a109987f32291a7c11dad1cfa8
Matt Carney <matt.carney50@gmail.com>
2023-07-23 18:49:57 -0400

    added sol irradiance to sensitivity,finalized

--- pages/02_Weather-Sensitivity.py
+++ pages/02_Weather-Sensitivity.py
@@ -6,7 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from scipy.stats import norm
-from utils import DLR, temp_data, gen_normal, gen_uniform, wind_direction_data
+from utils import DLR, temp_data, gen_normal, gen_uniform, wind_direction_data, solar_irr_data
 
 st.title('Probabilistic Weather - Sensitivity')
 #st.markdown('This is some text')
@@ -14,9 +14,18 @@ st.title('Probabilistic Weather - Sensitivity')
 
 
 ### Sidebar Sliders
+
+# Solar Irradiance
+with st.sidebar:
+    st.write("Solar Irradiance Variables")
+sol_decay = st.sidebar.slider('Solar Temporal Decay', 0.0, 1.0, 1.0)
+sol_irr = st.sidebar.slider('Heat Flux (W)', 0, 2000, 1000)
+sol_perc = st.sidebar.slider('Percent Cloud Cover', 0.0, 1.0, 0.5)
+
+# Wind Direction
 with st.sidebar:
     st.write("Wind Direction Variables")
-dir_decay = st.sidebar.slider('Temporal Decay', 0.0, 1.0, 1.0)
+dir_decay = st.sidebar.slider('Wind Dir. Temporal Decay', 0.0, 1.0, 1.0)
 dir_deg = st.sidebar.slider('Degrees From Transmission Line Axis', 0, 360, 45)
 dir_kap = st.sidebar.slider('Kappa', 0.5, 10.0, 2.0)
 dir_temp_decay = np.linspace(0.0, dir_decay, 5)
@@ -27,6 +36,37 @@ dir_temp_decay = np.linspace(0.0, dir_decay, 5)
 
 
 
+### Solar Irradiance
+# Generate Data
+d_sol, t = solar_irr_data(sol_irr=sol_irr, sol_perc=sol_perc, decay=sol_decay)
+dists = d_sol.reshape(-1)
+
+# Plot
+my_colorsc=[[0.0, 'rgb(231, 236, 239)'],
+            [0.1, 'rgb(70, 107, 227)'],
+            [0.2, 'rgb(62, 155, 254)'],
+            [0.3, 'rgb(24, 214, 203)'], 
+            [0.4, 'rgb(70, 247, 131)'],
+            [0.5, 'rgb(162, 252, 60)'],
+            [0.6, 'rgb(225, 220, 55)'], 
+            [0.7, 'rgb(253, 165, 49)'],
+            [0.8, 'rgb(239, 90, 17)'], 
+            [0.9, 'rgb(196, 37, 2)'],
+            [1, 'rgb(122, 4, 2)']]
+
+fig_sol = go.Figure(go.Histogram2d(
+        x = t,
+        y = dists,
+        histnorm='percent',
+        colorscale='turbo',
+        autobinx=False,
+        xbins= dict(size= .264),
+        autobiny=False,
+        ybins= dict(size = 40)
+        ))
+fig_sol.update_layout(height=400, width=900, title_text="Solar Irradiance")
+
+st.plotly_chart(fig_sol)
 
 
 
@@ -56,7 +96,7 @@ for i in range(d_dir.shape[1]):
     elif i >= 96:
         d_dir[:,i] = d_4
 
-# Figure
+# Plot
 fig_wind = make_subplots(rows=1, cols=5, specs=[[{'type': 'polar'},
                                                 {'type': 'polar'},
                                                 {'type': 'polar'},
